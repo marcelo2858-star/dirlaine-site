c759fe0be59791d7137bb153556c18f57666df18
marcelo2858-star <marcelo2858@gmail.com>
2026-07-25 14:22:09 -0400

    Update Supabase configuration values

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.DIRLAINE_CONFIG = {
-  supabaseUrl: "https://tcadrzxqpthxbibisiog.supabase.co",
-  publishableKey: "sb_publishable_IbVH43slzRNYetZ5kvrdPw_m0Xj9lYV"
+  supabaseUrl: "https://cpnvkjmpmcidyddkdzp.supabase.co",
+  publishableKey: "sb_publishable_Y5NNqbTh2nJ7rR3NWnv-JA_S0CvSgvk"
 };
